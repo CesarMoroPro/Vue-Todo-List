941cdbad2a94203d89eb6cc8d644df6939363eda
César MORO <cesar.moro.dev@gmail.com>
2023-08-06 15:48:50 +0200

    Installation de la librairie Font-Awesome Vue3

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 // import './assets/main.css'
 
+//! VUE ET PINIA
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router/router-index';
 
+const app = createApp(App);
+
+//! VUETIFY
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -14,12 +18,21 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
+
+//! FONT-AWESOME
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+/* add icons to the library */
+library.add(faUserSecret);
 
 
-
-const app = createApp(App)
-
+//! MONTAGE DE L'APPLICATION
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
